ac1bd4ddaf816a024786b5516706806c1fe96c07
Kevin Szuchet <kevinszuchet@gmail.com>
2019-06-11 23:37:57 -0300

    Primer intento de aproximacion a la solucion

--- src/main/scala/MusicParser.scala
+++ src/main/scala/MusicParser.scala
@@ -6,30 +6,75 @@ case class Note(name: String)
 
 class MusicParser(input: String) {
   protected val inputStream = new PushbackReader(new StringReader(input))
+  private var patronParser: PatronParser = null
 
   protected def parseChar(): Char = {
     val parsed = inputStream.read()
     if (parsed == -1) throw new EOIParserException
     return parsed.toChar
   }
-
-  protected def parseNote(): Nota = {
+  protected def parseNote(): List[Nota] = {
     var next: Char = ' '
     do next = parseChar() while (next == ' ')
-    Nota.notas.find(_.toString == next.toString()).getOrElse(throw new NotANoteException(next))
+
+    if (next.isDigit) {
+      var repeticiones: Int = 0
+      do {
+        repeticiones = (repeticiones * 10) + next.asDigit
+        next = parseChar()
+        if (!next.isDigit && next != 'x') throw new NotAValidPatronException(next)
+      } while (next.toLower != 'x')
+
+      patronParser = new PatronParser(repeticiones)
+
+      next = parseChar()
+      if (next != '(') throw new NotAValidPatronException(next)
+
+      println("Entro al patron")
+      patronParser.searchPatron(this)
+      return List.empty
+    }
+
+    if (next == ')') {
+      val notas = patronParser.parse()
+      println("Devuelve el control al music parser", notas)
+      return notas
+    }
+
+    List[Nota](Nota.notas.find(_.toString == next.toString()).getOrElse(throw new NotANoteException(next)))
   }
 
   def parse(): List[Nota] = {
-    val result: ListBuffer[Nota] = ListBuffer()
-    try while (true)
-      result += parseNote()
+    var result: List[Nota] = List()
+    try while (true) {
+      val notas: List[Nota] = parseNote()
+      println("Las notas", notas)
+      result = result ::: notas
+    }
     catch {
       case _: EOIParserException =>
     }
-    return result.toList
+    return result
+  }
+}
+
+class PatronParser(repeticiones: Int = 1) {
+  protected var patron: List[Nota] = List[Nota]()
+
+  def searchPatron(musicParser: MusicParser): Unit = {
+    patron = patron ::: musicParser.parse()
+    println("Patron completo", patron)
+  }
+
+  def parse(): List[Nota] = {
+    println("Sale del patron", patron)
+    val patronRepetido: List[Nota] = List.fill(repeticiones)(patron.toList).flatten
+    patron = List[Nota]()
+    return patronRepetido
   }
 }
 
 class ParserException(reason: String) extends Exception(reason)
 class EOIParserException extends ParserException("reached end of input")
 class NotANoteException(val read: Char) extends ParserException(s"Expected [A|B|C|D|E|F|G] but got $read")
+class NotAValidPatronException(val read: Char) extends ParserException(s"Expected NUMx([A|B|C|D|E|F|G]+) but got $read")
--- src/main/scala/PlayAudio.scala
+++ src/main/scala/PlayAudio.scala
@@ -1,5 +1,6 @@
 object PlayAudio extends App {
-  val sariaSong = "F A B B F A B B F A B E D D B C B G E E D D E G E F A B B F A B B F A B E D D B C E B G G D E G E"
+  val sariaSong = "F A 2x(C D E) B B"
+  // val sariaSong = "F A B B F A B B F A B E D D B C B G E E D D E G E F A B B F A B B F A B E D D B C E B G G D E G E"
 
   AudioPlayer.reproducir(sariaSong)
 }
